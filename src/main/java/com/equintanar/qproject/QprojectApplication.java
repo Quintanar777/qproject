5e116abe65d767c4825d9e6a4e9e51d74311235b
Erick Quintanar <erick.quintanar@microfocus.com>
2017-04-27 11:48:40 -0500

    commit initial project

---
+++ src/main/java/com/equintanar/qproject/QprojectApplication.java
@@ -0,0 +1,12 @@
+package com.equintanar.qproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QprojectApplication.class, args);
+	}
+}
